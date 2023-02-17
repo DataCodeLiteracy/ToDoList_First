4649b9b8b033d3e60f44d1e5c9dd72be12ea85ae
DataCodeLiteracy <103319477+DataCodeLiteracy@users.noreply.github.com>
2023-02-18 04:10:09 +0900

    JS Convert span to input

--- ToDoList.js
+++ ToDoList.js
@@ -1,3 +1,4 @@
+// navbar Sliding
 const $logoBars = document.querySelector(".logoBars");
 const $navbar = document.querySelector(".navbar");
 const $mainBody = document.querySelector(".mainBody");
@@ -5,4 +6,21 @@ const $mainBody = document.querySelector(".mainBody");
 $logoBars.addEventListener("click", function () {
   $navbar.classList.toggle("active");
 });
+
+// ------------------------------------------------------------
+
+// span -> input
+
+const spanBox = document.querySelector(".txt");
+const inputBox = document.createElement("input");
+inputBox.classList.add("inputSearch");
+
+spanBox.addEventListener("click", function () {
+  spanBox.replaceWith(inputBox);
+});
+
+inputBox.addEventListener("blur", function () {
+  inputBox.replaceWith(spanBox);
+});
+
 // ------------------------------------------------------------
